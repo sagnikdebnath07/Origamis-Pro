fbeeb385c2c521b81a4ca1b33bba0bf1c5d39052
Sagnik Debnath <148639195+sagnikdebnath07@users.noreply.github.com>
2025-03-04 17:14:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+def main():
+    st.title("Chatbot")
+    if "history" not in st.session_state:
+        st.session_state["history"] = []
+
+    from chain import handle_chat, createAgent
+    st.sidebar.title("Chatbot")
+    st.sidebar.write("This is a chatbot that can answer questions based on a given context. You can enter url for rag here.")
+    url=st.sidebar.text_input("Enter your message:", key="url")
+    if st.sidebar.button("Submit"):
+        app,config=createAgent(url)
+        st.session_state["history"] = []
+        st.session_state["app"]=app
+        st.session_state["config"]=config
+
+    # Function to handle message sending
+    def send_message():
+        if st.session_state.user_input:
+            user_message = st.session_state.user_input
+            if user_message=="quit" or user_message=="exit":
+                st.session_state["history"].append(("You", user_message))
+                st.session_state["history"].append(("CT", "Goodbye!"))
+                st.stop()
+            app=st.session_state["app"]
+            config=st.session_state["config"]
+            response = handle_chat(user_message,app,config)
+            st.session_state["history"].append(("You", user_message))
+            st.session_state["history"].append(("CT", response))
+            st.session_state.user_input = ""
+
+    user_input = st.text_input(
+        "Enter your message:", key="user_input", on_change=send_message
+    )
+
+    for idx, (user, message) in enumerate(reversed(st.session_state["history"])):
+        if user == "You":
+            st.markdown(f"<div style='text-align: right;'><b>You:</b> {message}</div>", unsafe_allow_html=True)
+        else:
+            st.markdown(f"<div style='text-align: left;'><b>CT:</b> {message}</div>", unsafe_allow_html=True)
+    st.divider()
+
+if __name__=="__main__":
+    main()
