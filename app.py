3058c99e5ca09ff826f6f5bf147c1580c1a9f578
Sagnik Debnath <148639195+sagnikdebnath07@users.noreply.github.com>
2025-03-05 18:59:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,29 @@ def main():
 
     from chain import handle_chat, createAgent
     st.sidebar.title("Chatbot")
-    st.sidebar.write("This is a chatbot that can answer questions based on a given context. You can enter url for rag here.")
+    st.sidebar.write("This is a chatbot that can answer questions based on a given context.")
+    st.sidebar.write("You can enter the URL for RAG here.")
     url=st.sidebar.text_input("Enter your url:", key="url")
+    default_url="https://en.wikipedia.org/wiki/Main_Page"
     with st.spinner("Loading"):
         if st.sidebar.button("Submit"):
-            app,config=createAgent(url)
-            st.session_state["history"] = []
-            st.session_state["app"]=app
-            st.session_state["config"]=config
-            st.sidebar.success("Chatbot is ready to use.")
-            
+            try:
+                app, config = createAgent(url)
+                st.session_state["history"] = []
+                st.session_state["app"] = app
+                st.session_state["config"] = config
+                st.sidebar.success("Chatbot is ready to use.")
+            except Exception as e:
+                st.sidebar.error(f"Error: {e}")
+
     # Function to handle message sending
     def send_message():
+        with st.spinner("Generating"):
+            if "app" not in st.session_state:
+                app, config = createAgent(default_url)
+                st.session_state["history"] = []
+                st.session_state["app"] = app
+                st.session_state["config"] = config
         if st.session_state.user_input:
             user_message = st.session_state.user_input
             if user_message.lower()=="quit" or user_message.lower()=="exit":
